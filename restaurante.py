c3089a9d3191d3b8c7d93e57c9d704ec18ce3a35
Antedeguemon1 <rodolfo.236.70@gmail.com>
2025-04-04 11:36:35 -0300

    acrescentei dicionarios e docstring

--- restaurante.py
+++ restaurante.py
@@ -4,48 +4,88 @@ restaurantes=[{"nome": "𝕔𝕒𝕓𝕖𝕔̧𝕒 𝕕𝕖 𝕘𝕖𝕝𝕠", "
               {"nome": "TI", 'categoria':"bar", "ativo":False}]
 
 def exibir_nome_do_programa():
+    '''essa função exibe o nome do restaurante'''
   
     print("𝕔𝕒𝕓𝕖𝕔̧𝕒 𝕕𝕖 𝕘𝕖𝕝𝕠")
-
+    
 def exibir_opções():
+
+    '''essa função exibe as opções'''
     print("1-cadastrar restaurante")
     print("2-listar restaurante")
-    print("3-ativar restaurante")
+    print("3-alterar status do restaurante") 
     print("4-encerrar programa")
        
 def Encerrando_programa():
+    '''essa função encerra o programa'''
     exibir_subtitulo("encerrando programa")
     voltar_ao_menu_principal()
     
 def opção_invalida():
+    '''essa função volta ao menu se for opção inválida'''
     print('Opção invalida"!\n')
     voltar_ao_menu_principal()
 
 def cadastrar_novo_restaurante():
+
+    '''essa função cadastra um novo restaurante'''
     exibir_subtitulo("cadastrando novo restaurante")
-    nome_do_restaurante = input('digite o nome do restaurante que deseja cadastrar:')
-    restaurantes.append(nome_do_restaurante)
+    nome_do_restaurante = input('digite o nome do restaurante que deseja cadastrar: ')
+    categoria = input (f'digite a categoria do restaurante {nome_do_restaurante} ')
+    dados_do_restaurante ={"nome": nome_do_restaurante,"categoria":categoria, 'ativo':False}
+    restaurantes.append(dados_do_restaurante)
     print(f'o restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
 
 def voltar_ao_menu_principal():
+      
+      '''essa função volta ao menu principal'''
       input("\nDigite qualquer tecla para voltar ao menu principal")
       main()
+    
+def alternar_estado_do_restaurante():
+
+    '''essa função alterna o estado do restaurante'''
+    exibir_subtitulo('alterando estado do restaurante')
+    nome_restaurante = input('digite o nome do restaurante que deseja alterar  o estado: ')
+    restaurante_encontrado = False
+    for restaurante in restaurantes:
+         if nome_restaurante == restaurante['nome']:
+              restaurante_encontrado = True
+              restaurante['ativo'] = not restaurante ['ativo']
+              mensagem = f'o restaurante {nome_restaurante} foi ativado com sucesso' if restaurante ["ativo"]else f'o restaurante{nome_restaurante} foi desativado com sucesso'
+              print (mensagem)
+    if not restaurante_encontrado:
+         print('o restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
 
 def listar_restaurantes():
+
+    '''essa função lista os restaurantes'''
     exibir_subtitulo("listando restaurantes")
 
+    
     for restaurante in restaurantes:
         nome_restaurante=restaurante['nome']
-        print(f' - .{nome_restaurante}')
+        categoria = restaurante["categoria"]
+        status = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f' - .{nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {status.ljust(20)}')
     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
+
+    '''essa função exibe os subtitulos'''
     os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def escolher_opção():
+
+    '''essa função serve para esccolher as opções disponiveis'''
     try:
        
         opção = int(input("escolha uma opção:"))
@@ -58,7 +98,7 @@ def escolher_opção():
         elif opção == 2:
               listar_restaurantes()
         elif opção == 3:
-                print("ativar restaurante")
+                alternar_estado_do_restaurante()
         elif opção == 4:
                 Encerrando_programa()
         else:
@@ -67,6 +107,8 @@ def escolher_opção():
           
           opção_invalida()
 def main():
+        
+        '''essa função inicia o programa'''
         os.system('cls')
         exibir_nome_do_programa()
         exibir_opções()
